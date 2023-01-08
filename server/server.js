393cc4e4d52a66b2da0c64a6ec5ae674e820d3aa
avgfx <avgfx15@gmail.com>
2023-01-08 19:22:29 +0530

    middlewares and dependency in server side

--- server/server.js
+++ server/server.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const app = express();
+
+// Config Secret for PORT
 const config = require("./config/configSecret");
 const port = config.port;
 
+// Database connection
 const dbConnect = require("./db/db");
+
+// Middlewares
+const cors = require("cors");
+const morgan = require("morgan");
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
 
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () =>
